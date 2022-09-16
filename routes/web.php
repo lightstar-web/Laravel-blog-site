7d7146ec4b4a4b0f6a08f99354843847b785322c
lightstar0412 <dev.star.man0412@gmail.com>
2022-09-16 12:15:07 +0100

    work with routes

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/blog', [PostsController::class, 'index']);
+//GET
+Route::get('/blog', [PostsController::class, 'index']);
+Route::get('/blog/1', [PostsController::class, 'show']);
 
-//Route for invoke method
+//POST
+Route::post('/blog/create', [PostsController::class, 'create']);
+Route::post('/blog', [PostsController::class, 'store']);
+
+//PUT OR PATCH
+Route::get('/blog/edit/1', [PostsController::class, 'edit']);
+Route::patch('/blog/1', [PostsController::class, 'update']);
+
+//DELETE
+Route::delete('/blog/1', [PostsController::class, 'destroy']);
+
+//Multiple HTTP verbs
+//Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
+//Route::any('/blog', [PostsController::class, 'index']);
+
+//Return a view
+//Route::view('/blog', 'blog.index', ['name' => 'Abdellatif laghjaj']);
+
 Route::get('/', HomeController::class);
+//Route for invoke method
 Route::resource('/blog', PostsController::class);
