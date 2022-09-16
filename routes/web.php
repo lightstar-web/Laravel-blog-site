6b6c947645f087612021ebab14df044263e08077
lightstar0412 <dev.star.man0412@gmail.com>
2022-09-16 01:43:30 +0100

    basics of controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/blog', [PostsController::class, 'index']);
+
+//Route for invoke method
+Route::get('/', HomeController::class);
+Route::resource('/blog', PostsController::class);
