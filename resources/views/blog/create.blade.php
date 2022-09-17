116d46d59abd54d1c7115575ecaa13fb8fc7fef6
lightstar0412 <dev.star.man0412@gmail.com>
2022-09-17 02:04:17 +0100

    create blog form

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -12,56 +12,60 @@
 <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="w-full max-w-md space-y-8">
         <div>
-            <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                 alt="Your Company">
-            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-            <p class="mt-2 text-center text-sm text-gray-600">
-                Or
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-            </p>
+            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Create a blog</h2>
         </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
+        <form class="mt-8 space-y-6" action="{{ route('blog.store') }}" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="remember" value="true">
             <div class="-space-y-px rounded-md shadow-sm">
-                <div>
-                    <label for="email-address" class="sr-only">Email address</label>
-                    <input id="email-address" name="email" type="email" autocomplete="email" required
+                <div class="my-2">
+                    <label for="title" class="sr-only">Title</label>
+                    <input id="title" name="title" type="text" autocomplete="title" required
                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                           placeholder="Email address">
+                           placeholder="Blog Title">
                 </div>
-                <div>
-                    <label for="password" class="sr-only">Password</label>
-                    <input id="password" name="password" type="password" autocomplete="current-password" required
-                           class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                           placeholder="Password">
+
+                <div class="my-2">
+                    <label for="excerpt" class="sr-only">Excerpt</label>
+                    <input id="excerpt" name="excerpt" type="text" autocomplete="excerpt" required
+                           class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                           placeholder="Blog Excerpt">
+                </div>
+
+                <div class="my-2">
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea id="body" name="body" type="text" autocomplete="body" required
+                              class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                              placeholder="Blog Body"></textarea>
+                </div>
+
+                <div class="my-2">
+                    <label for="category" class="sr-only">Minutes to read</label>
+                    <input id="min_to_read" name="min_to_read" type="number" min="1" autocomplete="min_to_read" required
+                           class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                           placeholder="Blog reading duration">
                 </div>
             </div>
 
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
-                    <input id="remember-me" name="remember-me" type="checkbox"
+                    <input id="is_published" name="is_published" type="checkbox"
                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                    <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+                    <label for="is_published" class="ml-2 block text-sm text-gray-900">Is Published</label>
                 </div>
+            </div>
 
-                <div class="text-sm">
-                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+            <div class="flex justify-center">
+                <div class="mb-3 w-96">
+                    <label for="formFile" class="form-label inline-block mb-2 text-gray-700">Blog image</label>
+                    <input class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile">
                 </div>
             </div>
 
             <div>
                 <button type="submit"
                         class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-          <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-            <!-- Heroicon name: mini/lock-closed -->
-            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd"
-                    d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                    clip-rule="evenodd"/>
-            </svg>
-          </span>
-                    Sign in
+                    Create blog
                 </button>
             </div>
         </form>
